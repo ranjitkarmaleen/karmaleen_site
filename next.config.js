5b6d141c1f959a44d2f1e5e5112ed9c2cb24cc32
Prins <urvashi.karmaln@gmail.com>
2025-12-10 17:27:33 +0530

    added .next and config changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // For Static Export
-  output: 'export',  // Enable static export
-  trailingSlash: true,
+  // output: 'export',  // Enable static export
+  // trailingSlash: true,
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
-  optimizeFonts: false, // Disable font optimization if needed
-  distDir: 'build',  // Specify build folder
+  // optimizeFonts: false, // Disable font optimization if needed
+  // distDir: 'build',  // Specify build folder
 }
 
 module.exports = nextConfig
